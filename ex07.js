c55db2cd4cca297ec1204ecee9a6d9d5cbf3d3c7
Jeff Elkner <jeff@elkner.net>
2015-07-22 11:38:16 -0400

    Added solution to exercise 7.

---
+++ ex07.js
@@ -0,0 +1,10 @@
+"use strict";
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function callback(response) {
+    response.setEncoding("utf-8");
+    response.on("data", function (data) {
+        console.log(data);
+    });
+});
